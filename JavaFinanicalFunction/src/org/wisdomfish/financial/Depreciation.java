167499132d4a88e3356f36cbe436f031c165650e
Kuo.ChaoYi <Kuo.ChaoYi@da0d8c7e-d689-11dd-a412-a1792178251d>
2008-12-31 01:22:53 +0000

    Modified package outline for javadoc.

---
+++ JavaFinanicalFunction/src/org/wisdomfish/financial/Depreciation.java
@@ -0,0 +1,24 @@
+package org.wisdomfish.financial;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+
+/**
+ * 折舊計算函數主要包括AMORDEGRC、AMORLINC、DB、DDB、SLN、SYD、VDB。
+ * 這些函數都是用來計算資產折舊的，只是採用了不同的計算方法。
+ * 這裏，對於具體的計算公式不再贅述，具體選用哪種折舊方法，則須視各單位情況而定。
+ * 
+ * @author ChaoYi ,Kuo
+ * @version 0.1
+ *
+ */
+public final class Depreciation {
+
+    private Depreciation() {
+    }
+
+}
---
+++ JavaFinanicalFunction/src/org/wisdomfish/financial/Invest.java
@@ -0,0 +1,30 @@
+package org.wisdomfish.financial;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+
+/**
+ * 投資計算函數可分為與未來值fv有關，與付款pmt有關，與現值pv有關，
+ * 與複利計算有關及與期間數有關幾類函數。
+ * 與未來值fv有關的函數--FV、FVSCHEDULE
+ * 與付款pmt有關的函數--IPMT、ISPMT、PMT、PPMT
+ * 與現值pv有關的函數--NPV、PV、XNPV
+ * 與複利計算有關的函數--EFFECT、NOMINAL
+ * 與期間數有關的函數--NPER
+ * 
+ * @author ChaoYi, Kuo
+ * @version 0.1
+ *
+ */
+public final class Invest {
+
+    private Invest() {
+    }
+
+
+
+}
