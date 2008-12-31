76efb57d8bcf3ee6b994bd14b336551564ddfc0d
Kuo.ChaoYi <Kuo.ChaoYi@da0d8c7e-d689-11dd-a412-a1792178251d>
2008-12-31 02:20:06 +0000

    Modified comment for javadoc.

--- trunk/JavaFinanicalFunction/src/org/wisdomfish/financial/Bond.java
+++ trunk/JavaFinanicalFunction/src/org/wisdomfish/financial/Bond.java
@@ -1,22 +1,20 @@
 package org.wisdomfish.financial;
 
-/**
- * 財務函數可以進行一般的財務計算，如確定貸款的支付額、投資的未來值或淨現值，以及債券或息票的價值。
- * 這些財務函數大體上可分為四類：投資計算函數、折舊計算函數、償還率計算函數、債券及其他金融函數。
- * 它們為財務分析提供了極大的便利。使用這些函數不必理解高級財務知識，只要填寫變數值就可以了。
- */
-
-
 import java.util.Date;
 
 /**
- * 債券及其他金融函數又可分為計算本金、利息的函數，與利息支付時間有關的函數、
- * 與利率收益率有關的函數、與修正期限有關的函數、
- * 與有價證券有關的函數以及與證券價格表示有關的函數。
+ * <ul>債券及其他金融函數可分為:
+ *  <li>計算本金、利息的函數</li>
+ *  <li>利息支付時間有關的函數</li>
+ *  <li>利率收益率有關的函數</li>
+ *  <li>修正期限有關的函數</li>
+ *  <li>有價證券有關的函數</li>
+ *  <li>證券價格表示有關的函數</li>
+ * </ul>
  * 
- * @author ChaoYi ,Kuo
+ * @author  ChaoYi ,Kuo
  * @version 0.1
- * @since http://office.microsoft.com/zh-tw/excel/HP052089791028.aspx
+ * @since http://sites.google.com/site/wisdomfishaccounting/java-accounting-api
  * 
  */
 public final class Bond {
@@ -26,15 +24,15 @@ public final class Bond {
 
     /**
      * 傳回定期給付利息債券所累積的利息。
-     * @param issue 為債券發行的日期。
-     * @param first_interest 為債券首次給付利息的日期。
-     * @param settlement 係指債券的結算日。債券結算日是債券交易給買方後的次日。
-     * @param rate 係指債券的年利率。
-     * @param par 為債券的面額。如果省略了 par，ACCRINT 將使用 1000 元做為面額。
-     * @param frequency 係指為每年給付利息的次數。<br />
-     * 若為每年給付一次，frequency = 1；若為每半年給付一次，frequency = 2；若為每季給付一次，frequency = 4。
-     * @param basis 係指所採用的日計利基的類型。
-     * @return 傳回定期給付利息債券所累積的利息。
+     * @param   issue 為債券發行的日期。
+     * @param   first_interest 為債券首次給付利息的日期。
+     * @param   settlement 係指債券的結算日。債券結算日是債券交易給買方後的次日。
+     * @param   rate 係指債券的年利率。
+     * @param   par 為債券的面額。如果省略了 par，ACCRINT 將使用 1000 元做為面額。
+     * @param   frequency 係指為每年給付利息的次數。<br />
+     *          若為每年給付一次，frequency = 1；若為每半年給付一次，frequency = 2；若為每季給付一次，frequency = 4。
+     * @param   basis 係指所採用的日計利基的類型。
+     * @return  傳回定期給付利息債券所累積的利息。
      */
     public static double ACCRINT(
             Date issue, Date first_interest, Date settlement,
--- trunk/JavaFinanicalFunction/src/org/wisdomfish/financial/Refund.java
+++ trunk/JavaFinanicalFunction/src/org/wisdomfish/financial/Refund.java
@@ -1,21 +1,18 @@
 package org.wisdomfish.financial;
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
-
 /**
  * 償還率計算函數主要用以計算內部收益率，包括IRR、MIRR、RATE和XIRR幾個函數。
  * 
- * @author ChaoYi, Kuo
+ * @author  ChaoYi, Kuo
  * @version 0.1
  */
 public final class Refund {
 
     private Refund() {
     }
+
+    public static void IRR() {}
+    public static void MIRR() {}
+    public static void XIRR() {}
 
 }
