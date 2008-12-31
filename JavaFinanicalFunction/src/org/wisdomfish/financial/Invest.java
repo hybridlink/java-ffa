990e157d13250036f4aed872b2adf2264ff39e54
Kuo.ChaoYi <Kuo.ChaoYi@da0d8c7e-d689-11dd-a412-a1792178251d>
2008-12-31 02:20:06 +0000

    Modified comment for javadoc.

--- JavaFinanicalFunction/src/org/wisdomfish/financial/Invest.java
+++ JavaFinanicalFunction/src/org/wisdomfish/financial/Invest.java
@@ -1,12 +1,5 @@
 package org.wisdomfish.financial;
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
-
 /**
  * 投資計算函數可分為與未來值fv有關，與付款pmt有關，與現值pv有關，
  * 與複利計算有關及與期間數有關幾類函數。
@@ -16,7 +9,7 @@ package org.wisdomfish.financial;
  * 與複利計算有關的函數--EFFECT、NOMINAL
  * 與期間數有關的函數--NPER
  * 
- * @author ChaoYi, Kuo
+ * @author  ChaoYi, Kuo
  * @version 0.1
  *
  */
@@ -25,6 +18,8 @@ public final class Invest {
     private Invest() {
     }
 
+    public static void FV() {}
+    public static void PV() {}
 
 
 }
