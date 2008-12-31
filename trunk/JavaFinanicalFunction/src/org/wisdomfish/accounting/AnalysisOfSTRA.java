86845218d308e670bdf6a57e1a83dd5b5214fb2b
Kuo.ChaoYi <Kuo.ChaoYi@da0d8c7e-d689-11dd-a412-a1792178251d>
2008-12-31 03:21:59 +0000

    Add Accounting - Analysis Of STRA.

---
+++ trunk/JavaFinanicalFunction/src/org/wisdomfish/accounting/AnalysisOfSTRA.java
@@ -0,0 +1,30 @@
+package org.wisdomfish.accounting;
+
+/**
+ * 短期償債能力分析(Analysis of Short-Term Repayment Ability)，
+ * 是針對被分析公司的現金與短期內可以變現的各種資產，以及短期內到期而必須償還的各種負債，進行評估。
+ * 企業可能獲利能力不錯，但卻可能因不能償還到期的負債而導致週轉不靈，以致無法繼續經營。
+ * 因此，短期償債能力分析，實是企業體質健全與否的重要指標。
+ *
+ * @author  ChaoYi, Kuo
+ * @version 0.1
+ */
+public class AnalysisOfSTRA {
+
+    private AnalysisOfSTRA() {
+    }
+
+    /**
+     * 流動比率(Current Ratio)流動比率越高，表示短期償債能力越高；流動比率越低，則短期償債能力越低。
+     * 流動資產中的某些項目在變現時可能有貶值之虞，因此推論出流動比率應超過200%方屬理想，
+     * 但也不可一概而論。一般來說只有100%左右；而如低於100%以下，則表示短期償債能力過低，對債權人的權益，已缺乏保障。
+     * 
+     * @param   currentAssets       流動資產
+     * @param   currentLiability    流動負債
+     * @return  回傳已格式化帶'%'符字串
+     */
+    public static String currentRatio(double currentAssets, double currentLiability) {
+        Double currentR = currentAssets / currentLiability * 100;
+        return currentR.toString() + "%";
+    }
+}
