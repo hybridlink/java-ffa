6fbb52616d3760c1c8e011fe221dc91f4ae68886
Kuo.ChaoYi <Kuo.ChaoYi@da0d8c7e-d689-11dd-a412-a1792178251d>
2009-01-02 21:43:24 +0000

    Add common interface 'DayCountBasis' and 'PV()' dev.

--- trunk/JavaFinanicalFunction/src/org/wisdomfish/common/BasisPart.java
+++ trunk/JavaFinanicalFunction/src/org/wisdomfish/common/BasisPart.java
@@ -2,25 +2,15 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package org.wisdomfish.common;
 
 import java.util.Date;
 
 /**
- * 定義需要使用到日期計算值的函數必需要實作的介面.
+ * 
  *
  * @version 0.1-dev, 2009-01
  * @author  ChaoYi, Kuo (Taiwan: 郭朝益)
  */
 public interface BasisPart {
-
-    /**
-     * 回傳二個日期點的天數差數值.
-     * @param initialDate   初始日
-     * @param finalDate     結束日
-     * @return  回傳二個日期點的天數差數值.
-     */
-    int daysBetween(Date initialDate, Date finalDate);
-
 }
---
+++ trunk/JavaFinanicalFunction/src/org/wisdomfish/common/DayCountBasis.java
@@ -0,0 +1,30 @@
+package org.wisdomfish.common;
+
+import java.util.GregorianCalendar;
+
+/**
+ * 與日期計算有關的函數類別必須實作本介面.
+ *
+ * @version 0.1-dev, 2009-01
+ * @author  ChaoYi, Kuo (Taiwan: 郭朝益)
+ */
+public interface DayCountBasis {
+
+    /**
+     * 給付時間點為每期的'開始".
+     */
+    public static final int AT_BEGINNING_OF_PERIOD = 1;
+    /**
+     * 給付時間點為每期的"期末".
+     */
+    public static final int AT_END_OF_PERIOD = 0;
+
+    /**
+     * 回傳二個日期點間的天數差值.
+     *
+     * @param initialDate   初始日
+     * @param finalDate     結束日
+     * @return  回傳二個日期點間的天數差值.
+     */
+    int getDaysBetween(GregorianCalendar initialDate, GregorianCalendar finalDate);
+}
