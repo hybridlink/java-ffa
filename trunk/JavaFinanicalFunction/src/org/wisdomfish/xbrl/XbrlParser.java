b996d5f57151999b27856ab2ce798b5acb173bf8
Kuo.ChaoYi <Kuo.ChaoYi@da0d8c7e-d689-11dd-a412-a1792178251d>
2009-01-15 20:50:05 +0000

    Add package - XBRL.

---
+++ trunk/JavaFinanicalFunction/src/org/wisdomfish/xbrl/XbrlParser.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.wisdomfish.xbrl;
+
+/**
+ * @version 0.1-dev, 01-2009
+ * @author ChaoYi ChaoYi, Kuo (Taiwan: 郭朝益)
+ */
+public class XbrlParser {
+
+}
